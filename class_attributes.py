a5d420819bfc083ea02f6e398498604ddceecc76
J-Phalen <jphalen335@comcast.net>
2025-06-02 21:48:13 -0400

    Update class_attributes.py

--- class_attributes.py
+++ class_attributes.py
@@ -34,22 +34,39 @@ class Student:
 
 # Create the first student object
 studentx = Student(1, "Jimmy")
+# Create the second student object
+studenty = Student(2, "Joi")
 
 # Add a class to them
 studentx.add_class("PCET")
+studenty.add_class("PCET")
 
-# Print out the attributes of the student
+# Print out the attributes of the first student
 name = studentx.get_name()
 attributes = studentx.display_attributes()
 print(f"The attributes of {name} are:")
 for k, v in attributes.items():
     print(f"{k}: {v}")
 
+# Print out the attributes of the second student
+name = studenty.get_name()
+attributes2 = studenty.display_attributes()
+print(f"The attributes of {name} are:")
+for k, v in attributes2.items():
+    print(f"{k}: {v}")
+
 # Remove that students name
 studentx.remove_student_name()
+studenty.remove_student_name()
 
-# Reprint the attributes of the student.
+# Reprint the attributes of the first student.
 print(f"The attributes of {name} are now:")
 attributes = studentx.display_attributes()
 for k, v in attributes.items():
+    print(f"{k}: {v}")
+
+# Reprint the attributes of the second student.
+print(f"The attributes of {name} are now:")
+attributes2 = studenty.display_attributes()
+for k, v in attributes2.items():
     print(f"{k}: {v}")
